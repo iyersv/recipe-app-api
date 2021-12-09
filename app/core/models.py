e03f373f7acb4134c539790b97fd385cc179134d
Satish Iyer <satish.iyer@landisgyr.com>
2021-12-09 13:17:23 -0600

    Added working list tags endpoint

--- app/core/models.py
+++ app/core/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.conf import settings
 
 
 class UserManager(BaseUserManager):
@@ -19,14 +20,14 @@ class UserManager(BaseUserManager):
 
         return user
 
-    def create_superuser(self,email,password):
+    def create_superuser(self, email, password):
         """
         Creates a new superuser
         :param email:
         :param password:
         :return:
         """
-        user = self.create_user(email,password)
+        user = self.create_user(email, password)
         user.is_staff = True
         user.is_superuser = True
         user.save(using=self.db)
@@ -46,3 +47,12 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
+
+
+class Tag(models.Model):
+    """ Tag to be used for recipe """
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def ___str__(self):
+        return self.name
