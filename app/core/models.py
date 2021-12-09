079fc2a0f30d0fc5996b8e29633e2bddbf7144f8
Satish Iyer <satish.iyer@landisgyr.com>
2021-12-09 17:32:23 -0600

    Added recipe

--- app/core/models.py
+++ app/core/models.py
@@ -67,3 +67,21 @@ class Ingredient(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Recipe(models.Model):
+    """  Recipe Model """
+
+    title = models.CharField(max_length=250)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    time_in_minutes = models.IntegerField()
+    price = models.DecimalField(max_digits=5,decimal_places=2)
+    link = models.CharField(max_length=255,blank=True)
+    ingredients = models.ManyToManyField('Ingredient')
+    tags = models.ManyToManyField('Tag')
+
+    def __str__(self):
+        return self.title
