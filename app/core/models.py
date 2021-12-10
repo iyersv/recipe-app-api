d2b094ff9f2115045bc12dc034d6e57d529742cc
Satish Iyer <satish.iyer@landisgyr.com>
2021-12-10 09:17:38 -0600

    Added image field to model

--- app/core/models.py
+++ app/core/models.py
@@ -2,6 +2,17 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.conf import settings
 
+import uuid
+import os
+
+
+def recipe_image_file_path(instance, filename):
+    """ Generate file path for new recipe image """
+    ext = filename.split('.')[-1]
+    filename = f'{uuid.uuid4()}.{ext}'
+
+    return os.path.join('uploads/recipe/', filename)
+
 
 class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
@@ -57,12 +68,13 @@ class Tag(models.Model):
     def ___str__(self):
         return self.name
 
+
 class Ingredient(models.Model):
     """ Ingredient to be used in a recipe """
     name = models.CharField(max_length=255)
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
-        on_delete= models.CASCADE
+        on_delete=models.CASCADE
     )
 
     def __str__(self):
@@ -78,10 +90,11 @@ class Recipe(models.Model):
         on_delete=models.CASCADE
     )
     time_in_minutes = models.IntegerField()
-    price = models.DecimalField(max_digits=5,decimal_places=2)
-    link = models.CharField(max_length=255,blank=True)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    link = models.CharField(max_length=255, blank=True)
     ingredients = models.ManyToManyField('Ingredient')
     tags = models.ManyToManyField('Tag')
+    image = models.ImageField(null=True, upload_to=recipe_image_file_path)
 
     def __str__(self):
         return self.title
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 from core import models
+from unittest.mock import patch
 
 
 def sample_user(email='test@abc.com', password="testpass"):
@@ -80,3 +81,12 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(str(recipe),recipe.title)
+
+    @patch('uuid.uuid4')
+    def test_recipe_filename_uuid(self,mock_uuid):
+        """" test that the image is served in the correct location """
+        uuid = 'test-uuid'
+        mock_uuid.return_value = uuid
+        file_path = models.recipe_image_file_path(None,'my_image.jpg')
+        exp_path = f'uploads/recipe/{uuid}.jpg'
+        self.assertEqual(file_path,exp_path)
