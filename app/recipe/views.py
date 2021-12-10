97bfa64bbd4aded051eb3c3b66be12f7633f103e
Satish Iyer <satish.iyer@landisgyr.com>
2021-12-09 18:08:11 -0600

    Added recipe feature

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,7 +1,7 @@
 from rest_framework import viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
-from core.models import Tag, Ingredient
+from core.models import Tag, Ingredient,Recipe
 from recipe import serializers
 
 
@@ -30,3 +30,16 @@ class TagViewSet(BaseRecipeAttrViewSet):
 class IngredientViewSet(BaseRecipeAttrViewSet):
     queryset = Ingredient.objects.all()
     serializer_class = serializers.IngredientSerializer
+
+class RecipeViewSet(viewsets.ModelViewSet):
+    """ Manage recipes in the database """
+
+    serializer_class =  serializers.RecipeSerializer
+    queryset = Recipe.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        """ Retrieve queryset for only the user authenticated """
+
+        return self.queryset.filter(user=self.request.user).order_by('-id')
